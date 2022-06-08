d9941f018e8708c8519af3a7d6092d8a59e1ad6c
bikcodeh <bikcodeh@gmail.com>
2022-06-08 06:56:37 -0500

    design map fragment and setup map

--- app/src/main/java/com/bikcodeh/distancetrackerapp/MapsFragment.kt
+++ app/src/main/java/com/bikcodeh/distancetrackerapp/MapsFragment.kt
@@ -1,11 +1,16 @@
 package com.bikcodeh.distancetrackerapp
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.bikcodeh.distancetrackerapp.databinding.FragmentMapsBinding
+import com.bikcodeh.distancetrackerapp.util.Extension.hide
+import com.bikcodeh.distancetrackerapp.util.Extension.show
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,27 +18,72 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
 
-    private val callback = OnMapReadyCallback { googleMap ->
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
 
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+    private lateinit var map: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapFragment?.getMapAsync(this)
+
+        with(binding) {
+            btnStar.setOnClickListener {
+
+            }
+
+            btnReset.setOnClickListener {
+
+            }
+
+            btnStop.setOnClickListener {
+
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.isMyLocationEnabled = true
+        map.setOnMyLocationButtonClickListener(this)
+        map.uiSettings.apply {
+            isZoomControlsEnabled = false
+            isZoomGesturesEnabled = false
+            isRotateGesturesEnabled = false
+            isCompassEnabled = false
+            isTiltGesturesEnabled = false
+            isScrollGesturesEnabled = false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        binding.tvMyLocation.animate().alpha(0f).duration = 1500
+        lifecycleScope.launch {
+            delay(2500)
+            binding.tvMyLocation.hide()
+            binding.btnStar.show()
+        }
+        return true
     }
 }
---
+++ app/src/main/java/com/bikcodeh/distancetrackerapp/util/Extension.kt
@@ -0,0 +1,19 @@
+package com.bikcodeh.distancetrackerapp.util
+
+import android.view.View
+import android.widget.Button
+
+object Extension {
+
+    fun View.show() {
+        this.visibility = View.VISIBLE
+    }
+
+    fun View.hide() {
+        this.visibility = View.INVISIBLE
+    }
+
+    fun Button.disable() {
+        isEnabled = false
+    }
+}
