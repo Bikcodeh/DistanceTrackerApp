4abc40e1853fc560a60c30b51d4fb400056a75a3
bikcodeh <bikcodeh@gmail.com>
2022-06-08 22:57:01 -0500

    create calculateDistance function

--- app/src/main/java/com/bikcodeh/distancetrackerapp/ui/maps/MapUtil.kt
+++ app/src/main/java/com/bikcodeh/distancetrackerapp/ui/maps/MapUtil.kt
@@ -2,6 +2,8 @@ package com.bikcodeh.distancetrackerapp.ui.maps
 
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import java.text.DecimalFormat
 
 object MapUtil {
 
@@ -19,5 +21,14 @@ object MapUtil {
         val hours = (elapsedTime / (1000 * 60 * 60) % 24)
 
         return "$hours:$minutes:$seconds"
+    }
+
+    fun calculateDistance(locations: List<LatLng>): String {
+        if (locations.isNotEmpty()) {
+            val meters = SphericalUtil.computeDistanceBetween(locations[0], locations.last())
+            val kilometers = meters / 1000
+            return DecimalFormat("#.##").format(kilometers)
+        }
+        return "0.00"
     }
 }
