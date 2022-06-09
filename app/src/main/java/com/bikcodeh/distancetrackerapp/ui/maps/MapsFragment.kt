13d39dca762da90480ebc14ee8c68bb23f6c4e98
bikcodeh <bikcodeh@gmail.com>
2022-06-08 23:07:53 -0500

    show total distance after stop tracking

--- app/src/main/java/com/bikcodeh/distancetrackerapp/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/bikcodeh/distancetrackerapp/ui/maps/MapsFragment.kt
@@ -26,10 +26,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.ButtCap
-import com.google.android.gms.maps.model.JointType
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import pub.devrel.easypermissions.AppSettingsDialog
@@ -107,7 +104,25 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
 
         TrackerService.stopTime.observe(viewLifecycleOwner) {
             stopTime = it
+            if ( stopTime != 0L) {
+                showBiggerPicture()
+            }
         }
+    }
+
+    private fun showBiggerPicture() {
+        val bounds = LatLngBounds.Builder()
+        for (location in locationList) {
+            bounds.include(location)
+        }
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds.build(),
+                100
+            ),
+            2000,
+            null
+        )
     }
 
     private fun drawPolyline(){
